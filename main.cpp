9529be5252bfb34760daedbe80fead9884fb3048
jackzajac7 <azajac1@mail.csuchico.edu>
2018-02-23 16:31:23 -0800

    grocery list

--- main.cpp
+++ main.cpp
@@ -1,19 +1,35 @@
+//Aero Zajac
 #include<iostream>
 #include<string>
-using namespace std;
-
+#include<vector>
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+        std::string list[5]; //array of 5 strings
+        int limit = 5;
+        int numItems = 0;
+        std::string item;
+        char input;
+        do {
+                std::cout<<"\n==GROCERY LIST MANAGER=="<<std::endl;
+                std::cout<<"\nEnter your choice: "<<std::endl;
+                std::cout<<"\n (A)dd an item"<<std::endl;
+                std::cout<<"\n (Q)uit"<<std::endl;
+                std::cout<<"\nYour choice (A/Q): "<<std::endl;
+                std::cin>>input;
+                if(input == 'A' || input == 'a') {
+                        if(limit > sizeof(list)/sizeof(*list)) { //i'm a little confused is this what you're asking for
+                                std::cout<< "You'll need a bigger list!" <<std::endl;
+                        }
+                        else{
+                                list[numItems] = item;
+                                numItems++;
+                        }
+                        std::cout<< "What is the item?" <<std::endl;
+                        std::cin>> item;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+                }
+        } while(input != 'Q' && input != 'q'); 
 
-  return 0;
+
+        return 0;
 }
