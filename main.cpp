9ad9401bb8980d7baf47f8d5d2fe733e22183f46
jackzajac7 <azajac1@mail.csuchico.edu>
2018-03-13 22:01:12 -0700

    New Grocery Store

--- main.cpp
+++ main.cpp
@@ -4,32 +4,39 @@
 #include<vector>
 int main()
 {
-        std::string list[5]; //array of 5 strings
-        int limit = 5;
-        int numItems = 0;
-        std::string item;
-        char input;
-        do {
-                std::cout<<"\n==GROCERY LIST MANAGER=="<<std::endl;
-                std::cout<<"\nEnter your choice: "<<std::endl;
-                std::cout<<"\n (A)dd an item"<<std::endl;
-                std::cout<<"\n (Q)uit"<<std::endl;
-                std::cout<<"\nYour choice (A/Q): "<<std::endl;
-                std::cin>>input;
-                if(input == 'A' || input == 'a') {
-                        if(limit > sizeof(list)/sizeof(*list)) { //i'm a little confused is this what you're asking for
-                                std::cout<< "You'll need a bigger list!" <<std::endl;
-                        }
-                        else{
-                                list[numItems] = item;
-                                numItems++;
-                        }
-                        std::cout<< "What is the item?" <<std::endl;
-                        std::cin>> item;
-
+    int numItems = 0;
+    std::vector<std::string> list; 
+    std::string item;
+    char input;
+    do {
+        std::cout<<"\n==GROCERY LIST MANAGER=="<<std::endl;
+        std::cout<<"\nEnter your choice: "<<std::endl;
+        std::cout<<"\n (A)dd an item"<<std::endl;
+        std::cout<<"\n (Q)uit"<<std::endl;
+        std::cout<<"\nYour choice (A/Q): "<<std::endl;
+        std::cin>>input;
+        if(input == 'A' || input == 'a') {
+            std::cout<< "What is the item?" <<std::endl;
+            std::cin>>item;
+            int size = list.size();
+                        //list.resize(size+=1);
+            list.push_back(item);
+        }
+        if(input == 'Q' || input == 'q') {
+            std::cout<< "==ITEMS TO BUY==" <<std::endl;
+            if(list.empty() == true) {
+                std::cout<< "No items to buy!" <<std::endl;
+            } 
+            else if(list.empty() == false) { 
+                for(int i=0; i<list.size();i++) {
+                    std::cout<< i+1 << " " << list[i] <<std::endl;
                 }
-        } while(input != 'Q' && input != 'q'); 
+
+            }
+
+        }
+    } while(input != 'Q' && input != 'q');
 
 
-        return 0;
+    return 0;
 }
